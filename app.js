ce5255977e456f78b1091764a3e33272b75c67cd
thiom <tomi.hnn@gmail.com>
2024-05-18 19:02:35 +0300

    11.12

--- app.js
+++ app.js
@@ -8,7 +8,11 @@ const PORT = process.env.PORT || 5001
 
 app.use(express.static('dist'))
 
-app.listen(PORT, () => {
+app.listen(PORT + 1, () => {
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
+})
+
+app.get('/health', (req, res) => {
+  res.send('ok')
 })
