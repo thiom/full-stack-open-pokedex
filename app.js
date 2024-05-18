9667997c423e22653887fcc0dcdcce6fb34c9b84
thiom <tomi.hnn@gmail.com>
2024-05-18 19:40:49 +0300

    11.12

--- app.js
+++ app.js
@@ -16,5 +16,5 @@ app.listen(PORT, () => {
 app.get('/health', (req, res) => {
   // eslint-disable-next-line no-constant-condition
   if (true) throw('error...  ')
-  res.send('ok2')
+  res.send('ok')
 })
